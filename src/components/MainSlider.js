8ed9923dd472c0f565cfd08a5596590fea358af5
Djosc <davidjoscaldwell@gmail.com>
2022-01-04 22:14:16 -0500

    Add functions to move between slides

--- src/components/MainSlider.js
+++ src/components/MainSlider.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styled, { css } from 'styled-components';
 import { Button } from './Button';
 import { BiRightArrowCircle } from 'react-icons/bi';
@@ -89,7 +89,9 @@ const SlideContent = styled.div`
 	}
 `;
 
-const Arrow = styled(BiRightArrowCircle)``;
+const Arrow = styled(BiRightArrowCircle)`
+	margin-left: 0.5rem;
+`;
 
 const SliderButtons = styled.div`
 	position: absolute;
@@ -126,35 +128,72 @@ const NextArrow = styled(BiRightArrowCircle)`
 `;
 
 const MainSlider = ({ slides }) => {
+	const [current, setCurrent] = useState(0);
+	const length = slides.length;
+	const timeout = useRef(null);
+
+	useEffect(() => {
+		const nextSlide = () => {
+			setCurrent((current) => (current === length - 1 ? 0 : current + 1));
+		};
+
+		timeout.current = setTimeout(nextSlide, 4000);
+		return function () {
+			if (timeout.current) {
+				clearTimeout(timeout.current);
+			}
+		};
+	}, [current, length]);
+
+	const nextSlide = () => {
+		if (timeout.current) {
+			clearTimeout(timeout.current);
+		}
+		setCurrent(current === length - 1 ? 0 : current + 1);
+	};
+
+	const prevSlide = () => {
+		if (timeout.current) {
+			clearTimeout(timeout.current);
+		}
+		setCurrent(current === 0 ? length - 1 : current - 1);
+	};
+
+	if (!Array.isArray(slides) || slides.length <= 0) {
+		return null;
+	}
+
 	return (
 		<SliderSection>
 			<SliderWrapper>
 				{slides.map((slide, index) => {
 					return (
 						<Slide key={index}>
-							<Slider>
-								<SlideImage src={slide.image} alt={slide.alt} />
-								<SlideContent>
-									<h1>{slide.title}</h1>
-									<p>{slide.description}</p>
-									<Button
-										to={slide.path}
-										primary="true"
-										css={`
-											max-width: 160px;
-										`}
-									>
-										{slide.label}
-										<Arrow />
-									</Button>
-								</SlideContent>
-							</Slider>
+							{index === current && (
+								<Slider>
+									<SlideImage src={slide.image} alt={slide.alt} />
+									<SlideContent>
+										<h1>{slide.title}</h1>
+										<p>{slide.description}</p>
+										<Button
+											to={slide.path}
+											primary="true"
+											css={`
+												max-width: 160px;
+											`}
+										>
+											{slide.label}
+											<Arrow />
+										</Button>
+									</SlideContent>
+								</Slider>
+							)}
 						</Slide>
 					);
 				})}
 				<SliderButtons>
-					<PrevArrow />
-					<NextArrow />
+					<PrevArrow onClick={prevSlide} />
+					<NextArrow onClick={nextSlide} />
 				</SliderButtons>
 			</SliderWrapper>
 		</SliderSection>
